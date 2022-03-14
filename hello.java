57b81b89c0d1bb6be9804e9ac949aa26eb313fed
w1nn1weng <zweng@ufl.edu>
2022-03-14 18:37:39 -0400

    hello

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        int a = 2;
+        int b = 3;
+        System.out.println(a + b);
     }
 }
